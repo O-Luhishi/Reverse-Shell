0023268151dae584ccf7e72bca509794a625ec95
Osama Luhishi <O-Luhishi@users.noreply.github.com>
2017-04-22 22:09:12 +0100

    Added files

---
+++ Server.py
@@ -0,0 +1,62 @@
+import socket
+import sys
+
+# Create Socket
+
+def socket_create():
+	try:
+		global host
+		global port
+		global s
+
+		host =  ''
+		port = 9999
+		s = socket.socket()
+
+	except socket.error() as msg:
+		print("Socket Creating Error: " + str(msg))
+
+# Bind socket to port and wait for connection from client
+
+def socket_bind():
+	try:
+		global host
+		global port
+		global s
+		print("Binding socket to port: " + str(port))
+		s.bind((host, port))
+		s.listen(5)
+
+	except socket.error as msg:
+		print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
+		socket_bind()
+
+# Establish a connection with client
+
+def socket_accept():
+	conn, address = s.accept()
+	print("Connection Has Been Established | " + " IP " + address[0] + " | Port " + str(address[1]))
+	send_commands(conn)
+	conn.close()
+
+# Send commands
+def send_commands(conn):
+	while True:
+		cmd = input()
+		if cmd == 'quit':
+			conn.close()
+			s.close()
+			sys.exit()
+
+		if len(str.encode(cmd)) > 0:
+			conn.send(str.encode(cmd))
+			client_response = str(conn.recv(1024), "UTF-8")
+			print(client_response, end = "")
+
+def main():
+	socket_create()
+	socket_bind()
+	socket_accept()
+
+
+main()
